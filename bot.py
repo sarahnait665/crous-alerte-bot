442b47df952ba63dcae0cd455f32f675cb319c75
sarahnait665 <sarahnona999@gmail.com>
2026-07-24 21:55:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,26 @@
 import os
 import requests
-import json
+from playwright.sync_api import sync_playwright
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-API_URL = "https://trouverunlogement.lescrous.fr/api/fr/search/47"
+URL = "https://trouverunlogement.lescrous.fr/tools/47/search?occupationModes=alone&bounds=2.4130316_48.6485333_2.4705092_48.6109217&locationName=%C3%89vry+%2891000%29"
 
-def send_message(text):
+def send(msg):
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={
-            "chat_id": CHAT_ID,
-            "text": text
-        }
+        data={"chat_id": CHAT_ID, "text": msg[:4000]}
     )
 
-payload = {
-    "page": 0,
-    "pageSize": 0,
-    "occupationModes": ["alone"]
-}
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-try:
-    response = requests.post(
-        API_URL,
-        json=payload,
-        timeout=20
-    )
+    page.goto(URL, wait_until="networkidle", timeout=60000)
 
-    data = response.json()
+    text = page.locator("body").inner_text()
 
-    send_message(
-        json.dumps(
-            data["aggregations"],
-            indent=2,
-            ensure_ascii=False
-        )[:4000]
-    )
+    browser.close()
 
-except Exception as e:
-    send_message("❌ Erreur : " + str(e))
+send(text)
