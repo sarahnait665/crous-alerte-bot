b0eac13cfecdb1f55a9351693b9f5aa2826bd10c
sarahnait665 <sarahnona999@gmail.com>
2026-07-24 22:04:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,16 @@ CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 URL = "https://trouverunlogement.lescrous.fr/tools/47/search?occupationModes=alone&bounds=2.4130316_48.6485333_2.4705092_48.6109217&locationName=%C3%89vry+%2891000%29"
 
-def send(msg):
+
+def send(message):
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        data={"chat_id": CHAT_ID, "text": msg[:4000]}
+        data={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
     )
+
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -23,4 +28,14 @@ with sync_playwright() as p:
 
     browser.close()
 
-send(text)
+
+if "Aucun logement trouvé pour Évry (91000)" in text:
+    pass
+else:
+    send(
+        "🚨🚨🚨 ALERTE CROUS 🚨🚨🚨\n\n"
+        "UN LOGEMENT EST APPARU À ÉVRY !\n\n"
+        "Ouvre immédiatement :\n"
+        "https://trouverunlogement.lescrous.fr/tools/47/search?occupationModes=alone&bounds=2.4130316_48.6485333_2.4705092_48.6109217&locationName=%C3%89vry+%2891000%29\n\n"
+        "⚠️ Dépêche-toi !"
+    )
