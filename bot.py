aa2fa2dc4370bdf358658ac7e915020f4b1732d5
sarahnait665 <sarahnona999@gmail.com>
2026-07-24 17:57:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import os
+import requests
 
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+message = "🤖 Ton bot CROUS fonctionne !"
+
+requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": message,
+    },
+)
