584b853262824029d82764352c566d092c7aa341
sarahnait665 <sarahnona999@gmail.com>
2026-07-24 20:02:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,31 @@ import requests
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-message = "🤖 Ton bot CROUS fonctionne !"
+API_URL = "https://trouverunlogement.lescrous.fr/api/fr/search/26"
 
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": message,
-    },
-)
+def send_message(text):
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
+
+try:
+    response = requests.get(API_URL, timeout=20)
+
+    if response.status_code == 200:
+        data = response.text[:500]
+        send_message(
+            "✅ API accessible !\n\nRéponse reçue :\n" + data
+        )
+    else:
+        send_message(
+            f"❌ API erreur HTTP : {response.status_code}"
+        )
+
+except Exception as e:
+    send_message(
+        "❌ Erreur : " + str(e)
+    )
